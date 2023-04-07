3c956ebfde0ed6a652c27044a5d95437eedea24f
khyle_siarot <johnkhyle.siarot.20@usjr.edu.ph>
2023-04-07 17:08:48 +0800

    seconds and minutes widgets

--- multiprovider/lib/main.dart
+++ multiprovider/lib/main.dart
@@ -1,19 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
 void main() {
   runApp(MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const HomePage(),
-    ));
+    title: 'Flutter Demo',
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+    ),
+    home: const HomePage(),
+  ));
 }
+
+String now() => DateTime.now().toIso8601String();
+
+@immutable
+class Seconds {
+  final String value;
+  Seconds() : value = now();
+}
+
+@immutable
+class Minutes {
+  final String value;
+  Minutes() : value = now();
+}
+
+Stream<String> newStream(Duration duration) =>
+    Stream.periodic(duration, (_) => now());
+
+
+
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold( 
+    return Scaffold(
       appBar: AppBar(title: const Text('Home Page')),
-);
+    );
+  }
+}
+
+
+class SecondsWidget extends StatelessWidget {
+  const SecondsWidget({Key? key}): super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final seconds = context.watch<Seconds>();
+    return Expanded(
+      child: Container(
+        color: Colors.yellow,
+        height: 100,
+      child: Text(seconds.value),
+      ),
+    );
+  }
+}
+
+class MinutesWidget extends StatelessWidget {
+  const MinutesWidget({Key? key}): super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final minutes = context.watch<Minutes>();
+    return Expanded(
+      child: Container(
+        color: Colors.blue,
+        height: 100,
+      child: Text(minutes.value),
+      ),
+    );
   }
 }
